c34899779e3dc6227ac33bc188e1aec5cc88735c
Sameera-Ahmed <121469203+Sameera-Ahmed@users.noreply.github.com>
2023-08-13 23:51:37 -0400

    Update Main.java

--- src/frames/Main.java
+++ src/frames/Main.java
@@ -1,15 +1,10 @@
 package frames;
 
-import classes.Book;
-import classes.Library;
-
 public class Main {
     public static Library library = new Library();
 
     public static void main(String[] args) {
         LibraryGUI libfrm = new LibraryGUI();
         libfrm.show();
-        
-        //Book b = new Book("Title", "author", "publisher", "synopsis", "type");
     }
 }
